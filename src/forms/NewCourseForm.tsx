9e6e58d31d8187a3427edd8d5aae7cab84661a00
Rinat Rezyapov <rinat.rezyapov@gmail.com>
2023-04-11 23:03:05 +0400

    Add errors to form fields

--- src/forms/NewCourseForm.tsx
+++ src/forms/NewCourseForm.tsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/native';
 import React, {FC} from 'react';
 import {Controller, useFieldArray, useForm} from 'react-hook-form';
-import {Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {Button, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {Asset, launchImageLibrary} from 'react-native-image-picker';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {FormMode} from '../types/forms';
-import {Asset, ImagePickerResponse, launchImageLibrary} from 'react-native-image-picker';
 
 export interface IFormFields {
   title: string;
@@ -36,7 +36,7 @@ const NewCourseForm: FC<IProps> = ({mode, defaultValues, onSubmit}) => {
     },
   });
 
-  const {fields, append, prepend, remove, swap, move, insert} = useFieldArray({
+  const {fields, append, remove} = useFieldArray({
     control,
     name: 'words',
   });
@@ -51,93 +51,93 @@ const NewCourseForm: FC<IProps> = ({mode, defaultValues, onSubmit}) => {
     });
   };
 
+  console.log(errors.title);
+
   return (
-    <Wrapper>
-      <ImageWithDetailsWrapper>
-        <ImageWrapper>
-          {image ? (
-            <StyledImage source={{uri: image.uri}} />
-          ) : (
-            <ImagePlaceholder>
-              <TouchableOpacity onPress={onChooseImageClick}>
-                <Icon name="add-photo-alternate" size={30} />
-              </TouchableOpacity>
-            </ImagePlaceholder>
-          )}
-        </ImageWrapper>
-        <DetailsWrapper>
-          <Controller
-            name="title"
-            rules={{required: true}}
-            control={control}
-            render={({field: {value, onChange, onBlur}}) => (
-              <TextInputNeo
-                style={styles.input}
-                placeholder="Label"
-                value={value}
-                onChangeText={value => onChange(value)}
-                onBlur={onBlur}
-              />
+    <ScrollView>
+      <Wrapper>
+        <ImageWithDetailsWrapper>
+          <ImageWrapper>
+            {image ? (
+              <StyledImage source={{uri: image.uri}} />
+            ) : (
+              <ImagePlaceholder>
+                <TouchableOpacity onPress={onChooseImageClick}>
+                  <Icon name="add-photo-alternate" size={30} />
+                </TouchableOpacity>
+              </ImagePlaceholder>
             )}
-          />
-          <Controller
-            name="description"
-            rules={{required: true}}
-            control={control}
-            render={({field: {value, onChange, onBlur}}) => (
-              <TextInputNeo
-                style={styles.input}
-                placeholder="Description"
-                value={value}
-                onChangeText={value => onChange(value)}
-                onBlur={onBlur}
+          </ImageWrapper>
+          <DetailsWrapper>
+            <FieldWithHelper>
+              <Controller
+                name="title"
+                rules={{required: {value: true, message: 'Required'}}}
+                control={control}
+                render={({field: {value, onChange, onBlur}}) => (
+                  <TextInputNeo
+                    placeholder="Label"
+                    value={value}
+                    onChangeText={value => onChange(value)}
+                    onBlur={onBlur}
+                  />
+                )}
               />
-            )}
-          />
-        </DetailsWrapper>
-      </ImageWithDetailsWrapper>
-      {fields.map((field, idx) => (
-        <FieldRowContainer key={field.id}>
-          <Controller
-            key={field.id}
-            name={`words.${idx}.value`}
-            rules={{required: true}}
-            control={control}
-            render={({field: {value, onChange, onBlur}}) => (
-              <FielInput
-                placeholder="Word"
-                style={styles.input}
-                value={value}
-                onChangeText={value => onChange(value)}
-                onBlur={onBlur}
+              {errors.title && <ErrorText>{errors.title.message}</ErrorText>}
+            </FieldWithHelper>
+            <FieldWithHelper>
+              <Controller
+                name="description"
+                rules={{required: {value: true, message: 'Required'}}}
+                control={control}
+                render={({field: {value, onChange, onBlur}}) => (
+                  <TextInputNeo
+                    placeholder="Description"
+                    value={value}
+                    onChangeText={value => onChange(value)}
+                    onBlur={onBlur}
+                  />
+                )}
               />
-            )}
-          />
-          <TouchableOpacity onPress={() => remove(idx)}>
-            <Icon name="remove" size={30} />
-          </TouchableOpacity>
-        </FieldRowContainer>
-      ))}
-      <AddTouchableWrapper onPress={() => append({value: ''})}>
-        <Icon name="add" size={30} />
-      </AddTouchableWrapper>
-      <Button title={mode === FormMode.update ? 'Update' : 'Create'} onPress={handleSubmit(onSubmit)} />
-    </Wrapper>
+              {errors.description && <ErrorText>{errors.description.message}</ErrorText>}
+            </FieldWithHelper>
+          </DetailsWrapper>
+        </ImageWithDetailsWrapper>
+        {fields.map((field, idx) => (
+          <FieldRowContainer key={field.id}>
+            <Controller
+              key={field.id}
+              name={`words.${idx}.value`}
+              rules={{required: {value: true, message: 'Required'}}}
+              control={control}
+              render={({field: {value, onChange, onBlur}}) => (
+                <FieldWithHelper>
+                  <TextInputNeo
+                    placeholder="Word"
+                    value={value}
+                    onChangeText={value => onChange(value)}
+                    onBlur={onBlur}
+                  />
+                  {errors.words?.[idx] && <ErrorText>{errors.words?.[idx]?.value?.message}</ErrorText>}
+                </FieldWithHelper>
+              )}
+            />
+
+            <TouchableOpacity onPress={() => remove(idx)}>
+              <Icon name="remove" size={30} />
+            </TouchableOpacity>
+          </FieldRowContainer>
+        ))}
+        <AddTouchableWrapper onPress={() => append({value: ''})}>
+          <Icon name="add" size={30} />
+        </AddTouchableWrapper>
+        <Button title={mode === FormMode.update ? 'Update' : 'Create'} onPress={handleSubmit(onSubmit)} />
+      </Wrapper>
+    </ScrollView>
   );
 };
 
 export default NewCourseForm;
-
-const styles = StyleSheet.create({
-  label: {
-    marginBottom: 8,
-  },
-  input: {
-    borderColor: 'grey',
-    borderWidth: 1,
-    borderRadius: 4,
-  },
-});
 
 const Wrapper = styled.View`
   display: flex;
@@ -154,17 +154,20 @@ const TextInputNeo = styled(TextInput)`
   border-width: 0;
   text-align-vertical: ${({multiline}) => (multiline ? 'top' : 'center')};
   padding: 16px;
-`;
-
-const FielInput = styled(TextInputNeo)`
-  flex: 1;
+  width: 200px;
 `;
 
 const FieldRowContainer = styled.View`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
   gap: 16px;
+`;
+
+const FieldWithHelper = styled.View`
+  display: flex;
+  flex-direction: column;
 `;
 
 const ImagePlaceholder = styled.View`
@@ -205,4 +208,10 @@ const DetailsWrapper = styled.View`
   display: flex;
   gap: 16px;
   flex: 3;
+`;
+
+const ErrorText = styled.Text`
+  color: red;
+  font-size: 10px;
+  margin: 8px 0 0 16px;
 `;
