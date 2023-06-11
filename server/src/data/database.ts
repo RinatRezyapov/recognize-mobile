042bd4697ca56a36910e1bb2c94521a39f9fc05f
Rinat Rezyapov <rinat.rezyapov@gmail.com>
2023-06-11 22:46:33 +0400

    Continue improve typings

--- server/src/data/database.ts
+++ server/src/data/database.ts
@@ -64,36 +64,49 @@ export class Score {
   }
 }
 
-export const getUser = (userId: number, pgPool: Pool) => {
-  return pgPool?.query(`SELECT * FROM users WHERE id='${userId}'`).then(response => {
+export class Streak {
+  userId: string;
+  courseId: string;
+  streak: number;
+  interval: number;
+  wordsCount: number;
+  constructor(userId: string, courseId: string, streak: number, interval: number, wordsCount: number) {
+    this.userId = userId;
+    this.courseId = courseId;
+    this.streak = streak;
+    this.interval = interval;
+    this.wordsCount = wordsCount;
+  }
+}
+
+export const getUser = (userId: number, pgPool: Pool) =>
+  pgPool?.query(`SELECT * FROM users WHERE id='${userId}'`).then(response => {
     const user = response.rows?.[0];
     return new User(user.id, user.email, user.username, user.first_login, user.last_login);
   });
-};
 
-export const getUserByEmail = (email: string, pgPool: Pool) => {
-  return pgPool?.query(`SELECT * FROM users WHERE email = '${email}'`).then(response => {
+export const getUserByEmail = (email: string, pgPool: Pool) =>
+  pgPool?.query(`SELECT * FROM users WHERE email = '${email}'`).then(response => {
     const user = response.rows?.[0];
     return new User(user.id, user.email, user.username, user.first_login, user.last_login);
   });
-};
 
-export const addUser = (email: string, username: string, pgPool: Pool) => {
-  return pgPool
+export const addUser = (email: string, username: string, pgPool: Pool) =>
+  pgPool
     ?.query(`INSERT INTO users (email, username) VALUES ('${email}', '${username}');`)
     .then(response => response.rows?.[0]);
-};
 
-export const getCourses = (userId: string, pgPool) => {
-  return pgPool?.query(`SELECT * FROM courses WHERE author_id = '${userId}'`).then(response => {
-    return response.rows.map(
-      v => new Course(v.id, v.title, v.description, v.body, v.created_at, v.updated_at, v.author_id, v.avatar),
+export const getCourses = (userId: string, pgPool) =>
+  pgPool
+    ?.query(`SELECT * FROM courses WHERE author_id = '${userId}'`)
+    .then(response =>
+      response.rows.map(
+        v => new Course(v.id, v.title, v.description, v.body, v.created_at, v.updated_at, v.author_id, v.avatar),
+      ),
     );
-  });
-};
 
-export const getCourse = (courseId: number, pgPool) => {
-  return pgPool?.query(`SELECT * FROM courses WHERE id = '${courseId}'`).then(response => {
+export const getCourse = (courseId: number, pgPool) =>
+  pgPool?.query(`SELECT * FROM courses WHERE id = '${courseId}'`).then(response => {
     const course = response.rows?.[0];
     return new Course(
       course.id,
@@ -106,10 +119,9 @@ export const getCourse = (courseId: number, pgPool) => {
       course.avatar,
     );
   });
-};
 
-export const addCourse = (pgPool, authorId, data) => {
-  return pgPool
+export const addCourse = (pgPool, authorId, data) =>
+  pgPool
     ?.query(
       `INSERT INTO courses (
     author_id,
@@ -130,62 +142,54 @@ export const addCourse = (pgPool, authorId, data) => {
     ) returning *;`,
     )
     .then(response => response.rows?.[0]);
-};
 
-export const updateCourse = (id: string, data, pgPool: Pool) => {
-  return pgPool
+export const updateCourse = (id: string, data, pgPool: Pool) =>
+  pgPool
     ?.query(
       `UPDATE courses SET title='${data.title}', description='${data.description}', body='${data.body}' WHERE id='${id}' RETURNING *;`,
     )
     .then(response => response.rows?.[0]);
-};
 
-export const removeCourse = (courseId: string, pgPool: Pool) => {
-  return pgPool?.query(`DELETE FROM courses WHERE id = '${courseId}'`).then(response => response.rows?.[0]);
-};
+export const removeCourse = (courseId: string, pgPool: Pool) =>
+  pgPool?.query(`DELETE FROM courses WHERE id = '${courseId}'`).then(response => response.rows?.[0]);
 
-export const getAllCourses = (pgPool: Pool, first: number) => {
-  return pgPool?.query(`SELECT * FROM courses LIMIT ${first || 2};`).then(response => {
-    return response.rows.map(
-      v => new Course(v.id, v.title, v.description, v.body, v.created_at, v.updated_at, v.author_id, v.avatar),
+export const getAllCourses = (pgPool: Pool, first: number) =>
+  pgPool
+    ?.query(`SELECT * FROM courses LIMIT ${first || 2};`)
+    .then(response =>
+      response.rows.map(
+        v => new Course(v.id, v.title, v.description, v.body, v.created_at, v.updated_at, v.author_id, v.avatar),
+      ),
     );
-  });
-};
 
-export const likeCourse = (userId: string, courseId: string, pgPool: Pool) => {
-  return pgPool
+export const likeCourse = (userId: string, courseId: string, pgPool: Pool) =>
+  pgPool
     ?.query(`INSERT INTO likes (user_id, course_id) VALUES ('${userId}', '${courseId}')`)
     .then(response => response.rows?.[0]);
-};
 
-export const unlikeCourse = (userId: string, courseId: string, pgPool: Pool) => {
-  return pgPool
+export const unlikeCourse = (userId: string, courseId: string, pgPool: Pool) =>
+  pgPool
     ?.query(`DELETE FROM likes WHERE course_id='${courseId}' AND user_id='${userId}'`)
     .then(response => response.rows?.[0]);
-};
 
-export const deleteCourseLikes = (courseId: string, pgPool: Pool) => {
-  return pgPool?.query(`DELETE FROM likes WHERE course_id='${courseId}'`).then(response => response.rows?.[0]);
-};
+export const deleteCourseLikes = (courseId: string, pgPool: Pool) =>
+  pgPool?.query(`DELETE FROM likes WHERE course_id='${courseId}'`).then(response => response.rows?.[0]);
 
-export const getCourseLikes = (id: string, pgPool: Pool) => {
-  return pgPool?.query(`SELECT * FROM likes where course_id='${id}';`).then(response => {
-    return response.rows;
-  });
-};
+export const getCourseLikes = (id: string, pgPool: Pool) =>
+  pgPool?.query(`SELECT * FROM likes where course_id='${id}';`).then(response => response.rows);
 
-export const getCourseScores = (id: string, pgPool: Pool) => {
-  return pgPool?.query(`SELECT * FROM scores where course_id='${id}';`).then(response => {
-    return response.rows.map(v => new Score(v.user_id, v.course_id, v.score, v.sequence, v.interval, v.words_count));
-  });
-};
+export const getCourseScores = (id: string, pgPool: Pool) =>
+  pgPool
+    ?.query(`SELECT * FROM scores where course_id='${id}';`)
+    .then(response =>
+      response.rows.map(v => new Score(v.user_id, v.course_id, v.score, v.sequence, v.interval, v.words_count)),
+    );
 
-export const getScore = (userId: string, courseId: string, pgPool: Pool) => {
-  return pgPool?.query(`SELECT * FROM scores where user_id='${userId}' AND course_id='${courseId}';`).then(response => {
+export const getScore = (userId: string, courseId: string, pgPool: Pool) =>
+  pgPool?.query(`SELECT * FROM scores where user_id='${userId}' AND course_id='${courseId}';`).then(response => {
     const v = response.rows?.[0];
     return new Score(v.user_id, v.course_id, v.score, v.sequence, v.interval, v.words_count);
   });
-};
 
 export const addScore = (
   userId: string,
@@ -195,21 +199,15 @@ export const addScore = (
   interval: number,
   wordsCount: number,
   pgPool: Pool,
-) => {
-  return pgPool
+) =>
+  pgPool
     ?.query(
       `INSERT into scores (user_id, course_id, score, sequence, interval, words_count) VALUES ('${userId}', '${courseId}', ${score}, '${sequence}', ${interval}, ${wordsCount});`,
     )
-    .then(response => {
-      return response.rows;
-    });
-};
+    .then(response => response.rows);
 
-export const deleteCourseScores = (courseId: string, pgPool: Pool) => {
-  return pgPool?.query(`DELETE FROM scores WHERE course_id='${courseId}'`).then(response => {
-    return response.rows;
-  });
-};
+export const deleteCourseScores = (courseId: string, pgPool: Pool) =>
+  pgPool?.query(`DELETE FROM scores WHERE course_id='${courseId}'`).then(response => response.rows);
 
 export const updateScore = (
   userId: string,
@@ -219,49 +217,42 @@ export const updateScore = (
   interval: number,
   wordsCount: number,
   pgPool: Pool,
-) => {
-  return pgPool
+) =>
+  pgPool
     ?.query(
       `UPDATE scores SET score=${score}, sequence='${sequence}', interval=${interval}, words_count=${wordsCount}  where user_id='${userId}' AND course_id='${courseId}';`,
     )
-    .then(response => {
-      return response.rows;
-    });
-};
+    .then(response => response.rows);
 
-export const getAllScores = (pgPool: Pool) => {
-  return pgPool?.query(`SELECT * FROM scores ORDER BY score asc;`).then(response => {
-    return response.rows.map(v => new Score(v.user_id, v.course_id, v.score, v.sequence, v.interval, v.words_count));
-  });
-};
+export const getAllScores = (pgPool: Pool) =>
+  pgPool
+    ?.query(`SELECT * FROM scores ORDER BY score asc;`)
+    .then(response =>
+      response.rows.map(v => new Score(v.user_id, v.course_id, v.score, v.sequence, v.interval, v.words_count)),
+    );
 
-export const getAllScoresWhere = (wordsCount: number, interval: number, pgPool: Pool) => {
-  return pgPool
+export const getAllScoresWhere = (wordsCount: number, interval: number, pgPool: Pool) =>
+  pgPool
     ?.query(`SELECT * FROM scores where words_count=${wordsCount} AND interval=${interval} ORDER BY score asc;`)
     .then(response =>
       response.rows.map(v => new Score(v.user_id, v.course_id, v.score, v.sequence, v.interval, v.words_count)),
     );
-};
 
-export const getCourseStreaks = (id: string, pgPool: Pool) => {
-  return pgPool?.query(`SELECT * FROM streaks where course_id='${id}';`).then(response => {
-    return response.rows;
+export const getCourseStreaks = (id: string, pgPool: Pool) =>
+  pgPool
+    ?.query(`SELECT * FROM streaks where course_id='${id}';`)
+    .then(response => response.rows.map(v => new Streak(v.user_id, v.course_id, v.streak, v.interval, v.words_count)));
+
+export const getAllStreaks = (pgPool: Pool) =>
+  pgPool
+    ?.query(`SELECT * FROM streaks ORDER BY streak asc;`)
+    .then(response => response.rows.map(v => new Streak(v.user_id, v.course_id, v.streak, v.interval, v.words_count)));
+
+export const getStreak = (userId: string, courseId: string, pgPool: Pool) =>
+  pgPool?.query(`SELECT * FROM streaks where user_id='${userId}' AND course_id='${courseId}';`).then(response => {
+    const v = response.rows?.[0];
+    return new Streak(v.user_id, v.course_id, v.streak, v.interval, v.words_count);
   });
-};
-
-export const getAllStreaks = (pgPool: Pool) => {
-  return pgPool?.query(`SELECT * FROM streaks ORDER BY streak asc;`).then(response => {
-    return response.rows;
-  });
-};
-
-export const getStreak = (userId: string, courseId: string, pgPool: Pool) => {
-  return pgPool
-    ?.query(`SELECT * FROM streaks where user_id='${userId}' AND course_id='${courseId}';`)
-    .then(response => {
-      return response.rows?.[0];
-    });
-};
 
 export const addStreak = (
   userId: string,
@@ -270,21 +261,15 @@ export const addStreak = (
   interval: number,
   wordsCount: number,
   pgPool: Pool,
-) => {
-  return pgPool
+) =>
+  pgPool
     ?.query(
       `INSERT into streaks (user_id, course_id, streak, interval, words_count) VALUES ('${userId}', '${courseId}', ${streak}, ${interval}, ${wordsCount});`,
     )
-    .then(response => {
-      return response.rows;
-    });
-};
+    .then(response => response.rows);
 
-export const deleteCourseStreak = (courseId: string, pgPool: Pool) => {
-  return pgPool?.query(`DELETE FROM streaks WHERE course_id='${courseId}'`).then(response => {
-    return response.rows;
-  });
-};
+export const deleteCourseStreak = (courseId: string, pgPool: Pool) =>
+  pgPool?.query(`DELETE FROM streaks WHERE course_id='${courseId}'`).then(response => response.rows);
 
 export const updateStreak = (
   userId: string,
@@ -293,12 +278,9 @@ export const updateStreak = (
   interval: number,
   wordsCount: number,
   pgPool: Pool,
-) => {
-  return pgPool
+) =>
+  pgPool
     ?.query(
       `UPDATE streaks SET streak=${streak}, interval=${interval}, words_count=${wordsCount} where user_id='${userId}' AND course_id='${courseId}';`,
     )
-    .then(response => {
-      return response.rows;
-    });
-};
+    .then(response => response.rows);
